4f3a23ddb65141ff90c70fbbaad6d17c38e1f8dd
jonathan <bayart.jonathan@laposte.net>
2022-05-27 15:40:15 +0200

    essai menu

---
+++ app.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("open").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("open").style.display = "block";
+}
